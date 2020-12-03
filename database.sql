cf72105ad533bdfcb0a79e76b60082cab5410fbc
Steven Maloney <steven.maloney@gmail.com>
2020-12-03 13:11:06 -0600

    queries done

--- database.sql
+++ database.sql
@@ -1,2 +1,17 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE shopping_list (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(80) NOT NULL, 
+	quantity FLOAT,
+    unit VARCHAR(20),
+	purchased BOOLEAN DEFAULT 'false'
+); 
+
+/*Test Data*/
+INSERT INTO shopping_list (name, quantity, unit)
+VALUES ('grapes', '2', 'lb’);
+
+INSERT INTO shopping_list (name, quantity, unit)
+VALUES ( 'cheese’', '1', 'lb');
