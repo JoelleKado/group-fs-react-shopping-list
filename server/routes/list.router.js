7400c82de1c6387ca43c70a7994ac599ee13b5df
Steven Maloney <steven.maloney@gmail.com>
2020-12-03 14:36:56 -0600

    might be working who knows

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -18,16 +18,16 @@ router.get('/', (req, res) => {
 
 //POST shopping items
 router.post('/', (req, res) => {
-  const newItem = req.body.newItem;
-    const sqlText = `INSERT INTO students (github_name) VALUES ($1) ($2) ($3)`;
-    pool.query(sqlText, [newItem.name], [newItem.qunatity], [newItem.unit])
-        .then((result) => {
-            res.sendStatus(201);
-        })
-        .catch((error) => {
-            console.log(`Error making database query ${sqlText}`, error);
-            res.sendStatus(500);
-        });
+  const newItem = req.body;
+  const sqlText = `INSERT INTO students (name, quantity, unit) VALUES ($1, $2, $3)`;
+  pool.query(sqlText, [newItem.name], [newItem.qunatity], [newItem.unit])
+     .then((result) => {
+        res.sendStatus(201);
+      })
+      .catch((error) => {
+          console.log(`Error making database query ${sqlText}`, error);
+          res.sendStatus(500);
+      })
 });
 
 
